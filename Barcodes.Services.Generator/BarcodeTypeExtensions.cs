3805b6b32378bca822ecc826e1127f620b24e8a4
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-01-05 12:45:04 +0100

    dev

--- Barcodes.Services.Generator/BarcodeTypeExtensions.cs
+++ Barcodes.Services.Generator/BarcodeTypeExtensions.cs
@@ -1,27 +1,28 @@
 ﻿using Aspose.BarCode.Generation;
 using System;
+using System.Collections.Generic;
 
 namespace Barcodes.Services.Generator
 {
     public static class BarcodeTypeExtensions
     {
+        public static Dictionary<BarcodeType, SymbologyEncodeType> CodesMap { get; set; } = new Dictionary<BarcodeType, SymbologyEncodeType>
+        {
+            { BarcodeType.Code128, EncodeTypes.Code128 },
+            { BarcodeType.DataMatrix, EncodeTypes.DataMatrix },
+            { BarcodeType.Ean128, EncodeTypes.GS1Code128 },
+            { BarcodeType.Ean13, EncodeTypes.EAN13 },
+            { BarcodeType.QRCode, EncodeTypes.QR },
+        };
+
         public static SymbologyEncodeType GetEncodeType(this BarcodeType barcodeType)
         {
-            switch (barcodeType)
+            if (!CodesMap.ContainsKey(barcodeType))
             {
-                case BarcodeType.Code128:
-                    return EncodeTypes.Code128;
-                case BarcodeType.DataMatrix:
-                    return EncodeTypes.DataMatrix;
-                case BarcodeType.Ean128:
-                    return EncodeTypes.GS1Code128;
-                case BarcodeType.Ean13:
-                    return EncodeTypes.EAN13;
-                case BarcodeType.QRCode:
-                    return EncodeTypes.QR;
-                default:
-                    throw new Exception("Invalid barcode type");
+                throw new Exception("Invalid barcode type");
             }
+
+            return CodesMap[barcodeType];
         }
     }
 }
---
+++ Barcodes.Utils/ListExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barcodes.Utils
+{
+    public static class ListExtensions
+    {
+        public static void ShiftLeft<T>(this List<T> list, int index)
+        {
+            if (list.Count < 2)
+                return;
+
+            if (index < 1)
+                return;
+
+            var temp = list[index - 1];
+            list[index - 1] = list[index];
+            list[index - 1] = temp;
+        }
+
+        public static void ShiftRight<T>(this List<T> list, int index)
+        {
+            if (list.Count < 2)
+                return;
+
+            if (index > list.Count - 2)
+                return;
+
+            var temp = list[index + 1];
+            list[index + 1] = list[index];
+            list[index + 1] = temp;
+        }
+    }
+}
