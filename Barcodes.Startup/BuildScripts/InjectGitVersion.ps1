ce7b772476790ec4b168fc2342dbc98cb61ffce9
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-02-17 14:13:54 +0100

    InjectGitVersion.ps1 was added

---
+++ Barcodes.Startup/BuildScripts/InjectGitVersion.ps1
@@ -0,0 +1,14 @@
+$gitVersion = git rev-list --all --count;
+$name = git rev-parse --abbrev-ref HEAD;
+$gitText= "{0}" -f [convert]::ToInt32($gitVersion, 10);
+
+$assemblyFile = $args[0] + "\Properties\AssemblyInfo.cs";
+$templateFile =  $args[0] + "\Properties\AssemblyInfo_Template.cs";
+
+$newAssemblyContent = Get-Content $templateFile |
+%{$_ -replace '0.0.0.0', ('0.9.5.' + $gitText) }
+
+If (-not (Test-Path $assemblyFile) -or ((Compare-Object (Get-Content $assemblyFile) $newAssemblyContent))) {
+    echo "Injecting Git Version Info to AssemblyInfo.cs"
+    $newAssemblyContent > $assemblyFile;       
+}
---
+++ Barcodes.Startup/Properties/AssemblyInfo_Template.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Barcodes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Barcodes")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
