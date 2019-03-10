8b4a921a8d6e2cc48bbf07d83e3fbd4f914e988b
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-03-10 14:46:20 +0100

    dversion 1.2.0

--- Barcodes.Startup/BuildScripts/InjectGitVersion.ps1
+++ Barcodes.Startup/BuildScripts/InjectGitVersion.ps1
@@ -6,7 +6,7 @@ $assemblyFile = $args[0] + "\Properties\AssemblyInfo.cs";
 $templateFile =  $args[0] + "\Properties\AssemblyInfo_Template.cs";
 
 $newAssemblyContent = Get-Content $templateFile |
-%{$_ -replace '0.0.0.0', ('1.1.0.' + $gitText) }
+%{$_ -replace '0.0.0.0', ('1.2.0.' + $gitText) }
 
 If (-not (Test-Path $assemblyFile) -or ((Compare-Object (Get-Content $assemblyFile) $newAssemblyContent))) {
     echo "Injecting Git Version Info to AssemblyInfo.cs"
