a2d48adc6419dd93d12878c975085183a545510c
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2020-05-25 14:07:56 +0200

    add PaletteCode

---
+++ Barcodes.Codes.Tests/PaletteCodeTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Barcodes.Codes.Tests
+{
+    [TestClass]
+    public class PaletteCodeTests
+    {
+        [TestMethod]
+        public void ValidFromString()
+        {
+            var codeString = "PA0001234";
+            var code = new PaletteCode(codeString);
+            Assert.AreEqual(1234, code.PaletteNumber);
+        }
+
+        [TestMethod]
+        public void ValidFromData()
+        {
+            var paletteNumber = 1234;
+            var code = new PaletteCode(paletteNumber);
+            code.CurrentPrefix = "PT";
+            Assert.AreEqual(1234, code.PaletteNumber);
+            Assert.AreEqual("PT1234", code.Code);
+        }
+
+        [TestMethod]
+        public void InvalidCodes()
+        {
+            var invalidCodes = new List<string>
+            {
+                "Px0001", // invalid prefix
+                "PA000A", // invalid character
+            };
+            foreach (var invalidCode in invalidCodes)
+            {
+                Assert.ThrowsException<ArgumentException>(() => new ContainerCode(invalidCode));
+            }
+        }
+
+        [TestMethod]
+        public void InvalidValues()
+        {
+            var values = new List<int>
+            {
+                -1, 0
+            };
+            foreach (var value in values)
+            {
+                Assert.ThrowsException<ArgumentException>(() => new ContainerCode(value));
+            }
+        }
+    }
+}
---
+++ Barcodes.Codes/PaletteCode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Barcodes.Codes
+{
+    public class PaletteCode : BaseCode
+    {
+        public PaletteCode()
+        {
+        }
+
+        public PaletteCode(string code)
+        {
+            Parse(code);
+        }
+
+        public PaletteCode(int number)
+        {
+            CheckNumber(number);
+            PaletteNumber = number;
+        }
+
+        public override string Code => CurrentPrefix + PaletteNumber.ToString();
+
+        public override BarcodeType Type => BarcodeType.Code128;
+
+        public override List<string> Prefixes => new List<string> { "PA", "PT" };
+
+        public int PaletteNumber { get; private set; } = 1;
+
+        private void CheckNumber(int number)
+        {
+            if (number < 1)
+            {
+                throw new ArgumentException("Invalid palette number");
+            }
+        }
+
+        public override void Parse(string code)
+        {
+            CheckCode(code);
+            var body = GetCodeBody(code);
+            CheckCodeOnlyDigits(body);
+            PaletteNumber = int.Parse(body);
+        }
+
+        public static bool TryParse(string code, out PaletteCode paletteCode)
+        {
+            paletteCode = null;
+            try
+            {
+                paletteCode = new PaletteCode(code);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
