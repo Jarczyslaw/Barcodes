4fc3dbd9964dfc8e589bc29f750d881c09219e6d
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2020-12-31 19:19:19 +0100

    InjectGitVersion updated

--- Barcodes.Startup/BuildScripts/InjectGitVersion.ps1
+++ Barcodes.Startup/BuildScripts/InjectGitVersion.ps1
@@ -1,14 +1,29 @@
+$appName = "Barcodes";
+$company = "JTJT";
+$version = "1.5.0";
+
 $gitVersion = git rev-list --all --count;
-$name = git rev-parse --abbrev-ref HEAD;
-$gitText= "{0}" -f [convert]::ToInt32($gitVersion, 10);
+$gitVersionText= "{0}" -f [convert]::ToInt32($gitVersion, 10);
+$branchName = git rev-parse --abbrev-ref HEAD;
+$hash = git rev-parse HEAD;
+$shortHash = git rev-parse --short HEAD;
 
 $assemblyFile = $args[0] + "\Properties\AssemblyInfo.cs";
-$templateFile =  $args[0] + "\Properties\AssemblyInfo_Template.cs";
+$templateFile = $args[0] + "\Properties\AssemblyInfo_Template.cs";
 
 $newAssemblyContent = Get-Content $templateFile |
-%{$_ -replace '0.0.0.0', ('1.5.0.' + $gitText) }
+%{$_ -replace "{appName}", ($appName) } |
+%{$_ -replace "{company}", ($company) } |
+%{$_ -replace "{version}", ($version + "." + $gitVersionText) } |
+%{$_ -replace "{branch}", ("Branch: " + $branchName) } |
+%{$_ -replace "{hash}", ("Hash: " + $hash) } |
+%{$_ -replace "{shortHash}", ("ShortHash: " + $shortHash) } |
+%{$_ -replace "{year}", (Get-Date -Format "yyyy") }
 
 If (-not (Test-Path $assemblyFile) -or ((Compare-Object (Get-Content $assemblyFile) $newAssemblyContent))) {
     echo "Injecting Git Version Info to AssemblyInfo.cs"
     $newAssemblyContent > $assemblyFile;       
+}
+Else {
+	echo "Injecting Git Version Info skipped"
 }
