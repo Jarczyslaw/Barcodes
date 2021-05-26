7074ab13a7e129776a5c83532e0a7bf0dafe586e
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2021-05-26 22:24:54 +0200

    new version

--- Barcodes.Startup/BuildScripts/InjectGitVersion.ps1
+++ Barcodes.Startup/BuildScripts/InjectGitVersion.ps1
@@ -1,6 +1,6 @@
 $appName = "Barcodes";
 $company = "JTJT";
-$version = "1.6.0";
+$version = "1.6.1";
 
 $gitVersion = git rev-list --all --count;
 $gitVersionText= "{0}" -f [convert]::ToInt32($gitVersion, 10);
